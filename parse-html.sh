41042dd4aa6dfafd73d067fef74555be1ea04afb
Jingbei Li <i@jingbei.li>
2016-09-23 17:30:17 +0800

    修改：     parse-html.sh

--- parse-html.sh
+++ parse-html.sh
@@ -2,5 +2,10 @@
 for i in html/*
 do
 	grep 'resize' -B1 $i > $i.tmp
+	sed \
+		-e '/^--$/d' \
+		-e 's/.*href="#\([^"]*\)".*/\1/g' \
+		-e 's/.*resize\.php?i=\([^_]*\).*alt=/\1\n/g' \
+		-e 's/ \/>$//' -i $i.tmp
 	mv $i.tmp $i
 done
