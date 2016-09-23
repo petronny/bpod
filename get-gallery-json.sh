cb326ea7e73783edfe1856a37936d3a4eabdd8fb
Jingbei Li <i@jingbei.li>
2016-09-23 14:45:29 +0800

    修改：     get-html.sh

---
+++ get-gallery-json.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+[ ! -d gallery-json ] && mkdir gallery-json
+lastest=`ls gallery-json | sort -g -r | head -n1| awk '{print $1}' | sed 's/\.json$//'`
+for i in `seq $lastest $(($lastest+1000))`
+do
+	echo $i
+	if [ ! -f gallery-json/$i.json ]
+	then
+		wget -O gallery-json/$i.json "http://www.bing.com/gallery/home/imagedetails/$i?z=0"
+		sleep 2
+	fi
+	[ `du gallery-json/$i.json | awk '{print $1}'` -eq 0 ] && rm gallery-json/$i.json
+done
--- get-html.sh
+++ get-html.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 [ ! -d html ] && mkdir html
+this_year=`date +'%Y'`
+this_month=`date +'%m'|sed 's/^0//'`
 download_html(){
 	echo $1-$2
+	[ -f html/$1-$2.html ] && return
 	wget -q -O html/$1-$2.html "http://www.istartedsomething.com/bingimages/?m=$2&y=$1#none"
 	sleep 10
 }
-
-this_year=`date +'%Y'`
 for year in `seq 2009 $this_year`
 do
 	if [ $year -eq $this_year ]
 	then
-		for month in `seq $(date +'%m')`
+		for month in `seq $this_month`
 		do
 			download_html $year $month
 		done
@@ -22,3 +23,5 @@ do
 		done
 	fi
 done
+
+wget -q -O html/$this_year-$this_month.html "http://www.istartedsomething.com/bingimages/?m=$2&y=$1#none"
