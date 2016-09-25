8c9723db187581be398676562f2aa76cd09adc95
Jingbei Li <i@jingbei.li>
2016-09-25 13:37:56 +0800

    will try 1920x1200 before 1920x1080

--- get-image.sh
+++ get-image.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-[ ! -d image ] && mkdir image
 download(){
 	date=`jq '.images[].startdate' $1|sed 's/"//g'`
 	url=`jq '.images[].url' $1|sed 's/"//g'`
-	mkdir image/$date
-	wget -P image/$date -c $url
-	url=`echo $url|sed 's/1920x1080/540x900/'`
-	wget -P image/$date -c $url
+	mkdir -p image/$date
+	filename=`echo $url | sed 's/.*\///'`
+	[ ! -f image/$date/`echo $filename| sed 's/1920x1080/1920x1200/'` ] && wget -P image/$date -c `echo $url|sed 's/1920x1080/1920x1200/'`
+	[ ! -f image/$date/`echo $filename| sed 's/1920x1080/1920x1200/'` ] && wget -P image/$date -c $url
+	[ ! -f image/$date/`echo $filename| sed 's/1920x1080/540x900/'` ] && wget -P image/$date -c `echo $url|sed 's/1920x1080/540x900/'`
 }
 [ -n "$1" ] && download $1 && exit
 for i in json/*.json
