d64474711f165bff47dbfca36c3c5ed5db3fc242
Jingbei Li <i@jingbei.li>
2016-10-14 03:33:44 +0800

    solve conflicts

--- calil.sh
+++ calil.sh
@@ -4,11 +4,7 @@ git reset --hard
 git pull
 for i in `seq 0 19`
 do
-<<<<<<< HEAD
-	date=`sh get-json.sh`
-=======
 	date=`sh get-json.sh $i`
->>>>>>> 9b8e2cbe9eb08959a240d6583d0ad2bcc2fd5d1e
 	sh get-image.sh json/$date.json
 done
 git add .
