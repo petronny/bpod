0633261530ba3fbfa07da3356c5b3c1c8a085744
Jingbei Li <i@jingbei.li>
2016-10-13 15:54:27 +0800

    fix all scripts

--- calil.sh
+++ calil.sh
@@ -2,8 +2,11 @@
 cd `dirname $0`
 git reset --hard
 git pull
-date=`sh get-json.sh`
-sh get-image.sh json/$date.json
+for i in `seq 0 19`
+do
+	date=`sh get-json.sh $i`
+	sh get-image.sh json/$date.json
+done
 git add .
 git commit -m "[$(LANG=C date)]auto update"
 git push
--- get-json.sh
+++ get-json.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 [ ! -d json ] && mkdir json
-wget -O tmp.json "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+[ -n "$1" ] && $1=0
+wget -O tmp.json "http://www.bing.com/HPImageArchive.aspx?format=js&idx=$1&n=1"
 date=`jq '.images[].startdate' tmp.json|sed 's/"//g'`
 mv tmp.json json/$date.json
 echo $date
