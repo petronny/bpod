37577298a62055a800199eaf0945b5e699c9fb40
Jingbei Li <i@jingbei.li>
2016-12-23 20:50:23 +0800

    fix image url

--- get-image.sh
+++ get-image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 download(){
 	date=`jq '.images[].startdate' $1|sed 's/"//g'`
-	url=`jq '.images[].url' $1|sed 's/"//g'`
+	url=`jq '.images[].url' $1|sed -e 's/"//g' -e 's/^\//http:\/\/www.bing.com\//'`
 	mkdir -p image/$date
 	filename=`echo $url | sed 's/.*\///'`
 	[ ! -f image/$date/`echo $filename| sed 's/1920x1080/1920x1200/'` ] && wget -P image/$date -c `echo $url|sed 's/1920x1080/1920x1200/'`
