d1642873b89858a33eeb398d20788d148198c6ce
Jingbei Li <i@jingbei.li>
2016-12-23 20:52:08 +0800

    fix get-json.sh

--- get-json.sh
+++ get-json.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 [ ! -d json ] && mkdir json
-[ -n "$1" ] && $1=0
-wget -O tmp.json "http://www.bing.com/HPImageArchive.aspx?format=js&idx=$1&n=1"
+[ -n "$1" ] && input=0 || input=$1
+wget -O tmp.json "http://www.bing.com/HPImageArchive.aspx?format=js&idx=$input&n=1"
 date=`jq '.images[].startdate' tmp.json|sed 's/"//g'`
 [ ! -f json/$date.json ] && mv tmp.json json/$date.json
 rm -f tmp.json
